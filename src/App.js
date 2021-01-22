f722f535df0255d4a78be1ea893efb58445aa104
Anthony Cortese <akcortese@Anthonys-MacBook-Pro.local>
2021-01-22 16:14:28 -0700

    completed sprint

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from "axios";
+import Character from "./components/Character"
+
 
 const App = () => {
+  const [names, setNames] = useState([]);
+  const [characterId, setCharacterId] = useState("")
+
+  const openDetails = id => {
+    setCharacterId(id)
+  }
+  
+  const closeDetails = () => {
+    setCharacterId(null)
+  }
+
+  useEffect(() => {
+    const getChar = () => {
+      axios.get(`https://swapi.dev/api/people/`)
+      .then(res => { 
+        setNames(res.data.results)
+        console.log(res.data.results)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    }
+    getChar()
+  }, []);
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -11,7 +38,12 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Star Wars Characters</h1>
+      {
+        names.map(na => {
+          return <Character bold key={na.id} info={na} charId={characterId} open={openDetails} close={closeDetails} />
+        })
+      }
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,66 @@
 // Write your Character component here
+import React, { useState, useEffect } from 'react'
+import axios from "axios"
+import styled from "styled-components";
+import Theme from "./Theme"
+
+const StyledProject = styled.div`
+text-align: center;
+h1 {
+  color: purple;
+  font-weight: bold;
+  text-align: center;
+  }
+
+  &:hover {
+    color: red;
+    background-color: royalblue;
+    transition: all 1.5s ease-in-out;
+  }
+
+img {
+  &:hover {
+    transform: scale(1.2);
+    transition: all 3s ease-in-out;
+  }
+}
+
+`;
+
+
+
+
+export default function Character(props) {
+    const { charId, close, info } = props;
+    const [details, setDetails] = useState({});
+
+    useEffect(() => {
+            axios.get(`https://swapi.dev/api/people/1/`)
+              .then(res => { 
+                  console.log(res.data);
+                  setDetails(res.data) 
+                })
+              .catch(err => { 
+                  console.log(err) 
+                })
+          }, []);
+    
+        return (
+            <StyledProject info={info} charId={charId} close={close}>
+            <div className= "input-group">
+                
+                    <h3>Name: {props.info.name}</h3>
+                    <p>Gender: {props.info.gender}</p>
+                    <p>Birth Year: {props.info.birth_year}</p>
+                    <p>Skin Color: {props.info.skin_color}</p>
+                    <p>Eye Color: {props.info.eye_color}</p>
+                    <p>Hair Color: {props.info.hair_color}</p>
+    
+                    
+                <button onClick={props.close}>Close</button>
+                
+                
+            </div>
+            </StyledProject>
+        )
+        }
